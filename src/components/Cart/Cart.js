9b0edeb878cac7b4c47c6a68e3d9ae25ff785b99
gitvhub <gitvhub@gmail.com>
2021-09-15 21:14:51 +0530

    fixed item handler logic

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -12,10 +12,12 @@ const Cart = (props) => {
   const hasItems = cartCtx.items.length > 0;
 
   const cartItemRemoveHandler = (id) => {
+    console.log("Add Item");
     cartCtx.removeItem(id);
   };
 
   const cartItemAddHandler = (item) => {
+    console.log("Removed Item");
     cartCtx.addItem({ ...item, amount: 1 });
   };
 
